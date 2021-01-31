48bb6842a45e90d1bf1cdd251c8642db51699ad9
kakatics <kakatics@gmail.com>
2021-01-31 07:49:37 +0100

    Implement Insertion Method

--- bst/BST.py
+++ bst/BST.py
@@ -1,5 +1,30 @@
 class Node(object):
   def __init__(self, data):
     self.data = data
-    self.left_child = None
-    self.right_child = None
+    self.leftChild = None
+    self.rightChild = None
+
+class BST(object):
+  def __init__(self):
+    slef.root = None;
+
+  def insert(self, data):
+    if not self.root:
+      self.root = Node(data)
+    else:
+      self.insertNode(data, self.root)
+
+  def insertNode(self, data, node):
+    if data == node.data:
+      return
+
+    if data < node.data:
+      if node.leftChild:
+        insertNode(data, node.leftChild)
+      else:
+        node.leftChild = Node(data)
+    else:
+      if node.rightChild:
+        insertNode(data, node.rightChild)
+      else:
+        node.rightChild = Node(data)
