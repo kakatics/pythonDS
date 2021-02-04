49f2dfc7a7f6115f584fc5f458374ef5e70aa796
kakatics <kakatics@gmail.com>
2021-02-04 17:23:48 +0100

    Implement Node class and AVL class with helper functions

---
+++ avl/AVL.py
@@ -0,0 +1,24 @@
+class Node(object):
+  def __init__(self, data):
+    self.data = data
+    self.height = 0
+    self.leftChild = None
+    self.rightChild = None
+
+
+class AVL(object):
+  def __init__(self):
+    self.root = None
+
+  def calcHeight(self, node):
+    if not node:
+      return -1
+
+    return node.height
+
+  def calcBalance(self, node):
+    if not node:
+      return 0
+
+    return self.calcHeight(node.leftChild) - self.calcHeight(node.rightChild)
+
