7d66aac3485334eac65778f192246df480d5d451
kakatics <kakatics@gmail.com>
2021-02-06 11:23:48 +0100

    Implement Rotation Functions

--- avl/AVL.py
+++ avl/AVL.py
@@ -22,3 +22,28 @@ class AVL(object):
 
     return self.calcHeight(node.leftChild) - self.calcHeight(node.rightChild)
 
+  def rotateRight(self, node):
+    print("Roatating to the right on node ", node.data)
+    
+    tempLeftChild = node.leftChild
+    t = tempLeftChild.rightChild
+    tempLeftChild.rightChild = node
+    node.leftChild = t
+    
+    node.height = max(self.calcHeight(node.leftChild), self.calcHeight(node.rightChild)) + 1
+    tempLeftChild.height = max(self.calcHeight(tempLeftChild.leftChild), self.calcHeight(tempLeftChild.rightChild)) + 1
+    
+    return tempLeftChild
+
+  def rotateLeft(self, node):
+    print("Roatating to the left on node ", node.data)
+    
+    tempRightChild = node.RightChild
+    t = tempRightChild.leftChild
+    tempRightChild.leftChild = node
+    node.RightChild = t
+    
+    node.height = max(self.calcHeight(node.leftChild), self.calcHeight(node.rightChild)) + 1
+    tempRightChild.height = max(self.calcHeight(tempRightChild.leftChild), self.calcHeight(tempRightChild.rightChild)) + 1
+
+    return tempRightChild
